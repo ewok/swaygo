2d8d49d1b52a5093d60f3faae9ea1e0cd0c90003
Artur Taranchiev <github@in.ewok.email>
2025-10-18 11:58:46 +0600

    try patch for mt7xxx

---
+++ files/scripts/patch-mt7xxx.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+sudo dnf5 -y install fedora-repos-archive
+sudo dnf5 -y install mt7xxx-firmware-20250808-1.fc42 --allowerasing
