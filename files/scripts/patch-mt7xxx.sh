e8d6da97be83d89cb210d6a1fde075bbf9b27080
Artur Taranchiev <github@in.ewok.email>
2025-11-30 00:59:33 +0600

    Migrate to 43 base

--- files/scripts/patch-mt7xxx.sh
+++ files/scripts/patch-mt7xxx.sh
@@ -4,4 +4,4 @@
 set -oue pipefail
 
 sudo dnf5 -y install fedora-repos-archive
-sudo dnf5 -y install mt7xxx-firmware-20250808-1.fc42 --allowerasing
+sudo dnf5 -y install mt7xxx-firmware-20250808-1.fc43 --allowerasing
